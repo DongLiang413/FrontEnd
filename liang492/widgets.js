22ae2d846a1887a78c409e7c164409311f94db34
Dong Liang <liang492@umn.edu>
2018-09-25 23:54:26 -0500

    done!

---
+++ liang492/widgets.js
@@ -0,0 +1,18 @@
+const ps = ["", "p1","p2","p3","p4","p5","p6","p7","p8","p9"];
+function move() {
+    var index = document.getElementById('cname').value;
+    if (index < 1 || index > 9) {
+        alert("Only 1 - 9 is allowed!");
+    } else {
+        var curSlot = ps[index];
+        var curColor = (document.getElementById('color').value).match(/\w+/);
+        document.getElementById(curSlot).style.backgroundColor = curColor;
+    }
+}
+
+function reset() {
+    var i = 1;
+    for (; i < 10; i++) {
+        document.getElementById(ps[i]).style.backgroundColor = "floralwhite";
+    }
+}
